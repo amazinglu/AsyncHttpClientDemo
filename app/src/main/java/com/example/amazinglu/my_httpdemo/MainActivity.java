400b16f9f58a1b23a58f38f354c5173269672ee7
AmazingLu <amazinglu166@gmail.com>
2017-11-08 21:40:32 -0500

    async_http frist commmit

--- app/src/main/java/com/example/amazinglu/my_httpdemo/MainActivity.java
+++ app/src/main/java/com/example/amazinglu/my_httpdemo/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.amazinglu.my_httpdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * use Android Asynchronous Http Client to do http get request
+         * */
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                TextView textView = (TextView) findViewById(R.id.text_view);
+                textView.setText(new String(responseBody));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                TextView textView = (TextView) findViewById(R.id.text_view);
+                textView.setText("Error , status code: " + statusCode);
+            }
+        });
     }
 }
