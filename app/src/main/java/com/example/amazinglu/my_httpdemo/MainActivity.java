38dafea08b24f0d5b1d5cc2eb882fa90bc070d93
AmazingLu <amazinglu166@gmail.com>
2017-11-08 21:58:05 -0500

    aync http frist commit

--- app/src/main/java/com/example/amazinglu/my_httpdemo/MainActivity.java
+++ app/src/main/java/com/example/amazinglu/my_httpdemo/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.amazinglu.my_httpdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Request request = new Request.Builder()
+//                .url("http://www.google.com")
+//                .build();
+//
+//        OkHttpClient client = new OkHttpClient();
+//        try {
+//            Response response = client.newCall(request).execute();
+//            Log.i("guojing_demo", response.body().string());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        /**
+         * use OkHttp to make synchronous HTTP request
+         * the request cannot run in mian thread
+         * */
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Request request = new Request.Builder()
+                .url("http://www.google.com")
+                .build();
+
+                OkHttpClient client = new OkHttpClient();
+                try {
+                    Response response = client.newCall(request).execute();
+                    Log.i("guojing_demo", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
